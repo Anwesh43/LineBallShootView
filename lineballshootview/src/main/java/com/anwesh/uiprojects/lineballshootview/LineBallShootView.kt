2ecf010297a27a83cfe9d73a177543a8b0e20c15
Anwesh43 <anweshthecool0@gmail.com>
2019-12-14 12:33:17 +0530

    adding Canvas extension methods

--- lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
+++ lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
@@ -20,8 +20,38 @@ val delay : Long = 30
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 3f
+val deg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineBallShoot(size : Float, scale : Float, w : Float, paint : Paint) {
+    val sc : Float = scale.divideScale(0, 2)
+    val sf : Float = sc.sinify()
+    val sc2 : Float = scale.divideScale(1, 2)
+    for (j in 0..1) {
+        save()
+        translate(size, 0f)
+        rotate(45f * sf * (1f - 2 * j))
+        drawLine(0f, 0f, -size, 0f, paint)
+        restore()
+    }
+    val r : Float = size / rFactor
+    drawCircle(size + (w - r - size) * sc2, 0f, r * sc, paint)
+}
+
+fun Canvas.drawLBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(0f, gap * (i + 1))
+    drawLineBallShoot(size, scale, w, paint)
+    restore()
+}
