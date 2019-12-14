3c6a8e7dd2a89ca0d4757fd91cd6f8bf20cc4ed6
Anwesh43 <anweshthecool0@gmail.com>
2019-12-14 12:48:55 +0530

    adding Animator class

--- lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
+++ lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
@@ -120,4 +120,46 @@ class LineBallShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSNode(var i : Int, val state : State = State()) {
+
+        private var next : LBSNode? = null
+        private var prev : LBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSNode {
+            var curr : LBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
