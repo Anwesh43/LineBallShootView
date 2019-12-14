da7c3ad89a1dec89af0ca246583bf7c55a1d3616
Anwesh43 <anweshthecool0@gmail.com>
2019-12-14 12:23:33 +0530

    adding constants and libraries

--- lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
+++ lineballshootview/src/main/java/com/anwesh/uiprojects/lineballshootview/LineBallShootView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.lineballshootview
  * Created by anweshmishra on 14/12/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 3f
+
