0c3673a286c3ce7bc044543bd34e8e53234c5ede
Anwesh43 <anweshthecool0@gmail.com>
2019-12-14 13:03:53 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlineballshootview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlineballshootview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlineballshootview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lineballshootview.LineBallShootView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineBallShootView.create(this)
     }
 }
